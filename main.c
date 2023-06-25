01ed05fda2c397cba19de064aadcae7a1ff0f5be
KM8Oz <ouazmir@gmail.com>
2023-06-25 18:12:51 +0100

    all

--- main.c
+++ main.c
@@ -1,32 +1,83 @@
 #include "monty.h"
+stack_t *head = NULL;
 
 /**
- * main - check code
+ * main - the function is the program's main entry point
  * @argc: arguments count
- * @argv: argument vetor
- *
- * Return: EXIT_SUCCESS, or EXIT_FAILURE if it failed
+ * @argv: list of arguments
+ * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	data_m data = {NULL, NULL, NULL, NULL, 0};
-	args_m args;
-
-	args.av = argv[1];
-	args.ac = argc;
-	args.line_number = 0;
-	if (args.ac != 2)
+	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, USAGE);
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	data.fptr = fopen(args.av, "r");
-	if (data.fptr == NULL)
-	{
-		dprintf(STDERR_FILENO, FILE_ERROR, args.av);
-		exit(EXIT_FAILURE);
-	}
-	execute(&args, data.fptr);
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
 
-	return (EXIT_SUCCESS);
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
